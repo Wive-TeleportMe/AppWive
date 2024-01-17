1c1de4c59fcc0004cb79f08b85393c2c35f48184
Ronpu <ronpu@hotmail.fr>
2024-01-17 11:24:08 +0100

    Recup image/video dans live (les bases)

--- Pages/LivePage.xaml.cs
+++ Pages/LivePage.xaml.cs
@@ -6,4 +6,96 @@ public partial class LivePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    // PHOTO PRISE SUR LE MOMENT AVEC L'APPLI PHOTO
+    private async void capturePhoto(Object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult myPhoto = await MediaPicker.Default.CapturePhotoAsync();
+            if (myPhoto != null)
+            {
+                // Sauvegarde l'image capturée dans l'application
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, myPhoto.FileName);
+
+                using Stream sourceStream = await myPhoto.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+                await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "ok");
+        }
+    }
+
+    // PHOTO PRISE DANS LES FICHIERS DU TEL
+    private async void takeImage(Object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult myPhoto = await MediaPicker.Default.PickPhotoAsync();
+            if (myPhoto != null)
+            {
+                // Sauvegarde l'image capturée dans l'application
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, myPhoto.FileName);
+
+                using Stream sourceStream = await myPhoto.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+                await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "ok");
+        }
+    }
+
+    // VIDEO PRISE SUR LE MOMENT AVEC L'APPLI PHOTO
+    private async void captureVideo(Object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            // POUR UNE IMAGE :
+            FileResult myVideo = await MediaPicker.Default.CaptureVideoAsync();
+            if (myVideo != null)
+            {
+                // Sauvegarde l'image capturée dans l'application
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, myVideo.FileName);
+
+                using Stream sourceStream = await myVideo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+                await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "ok");
+        }
+    }
+
+    // VIDEO PRISE DANS LES FICHIERS DU TEL
+    private async void takeVideo(Object sender, EventArgs e)
+    {
+
+        // TAKE PHOTO OR CAPTURE PHOTO
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            // POUR UNE IMAGE :
+            FileResult myVideo = await MediaPicker.Default.PickVideoAsync();
+            if (myVideo != null)
+            {
+                // Sauvegarde l'image capturée dans l'application
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, myVideo.FileName);
+
+                using Stream sourceStream = await myVideo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+                await sourceStream.CopyToAsync(localFileStream);
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("OOPS", "Your device isn't supported", "ok");
+        }
+    }
 }
